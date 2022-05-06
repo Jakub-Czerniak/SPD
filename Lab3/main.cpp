92d4238f7dc413a4d570b351289ea8b15df55706
Kuba <kubaczarny3@gmail.com>
2022-05-06 17:34:36 +0200

    working but not taking task with bigest weight

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cmath>
 #include <vector>
+#include <queue>
 
 //bez akcerelacji n^3 * n
 //akcerelacja obliczenie najdluzszej sciezki 3n^2*m
@@ -26,7 +27,8 @@ int main()
     bool usedTaskID[500];
     std::vector <int> order;
     std::vector <int> tempOrder;
-    int dataCount, taskCount, machineCount, cmax, min = 0;  
+    std::priority_queue <std::pair<int,int>> weightedTasks;
+    int dataCount, taskCount, machineCount, cmax, min, minTaskID, weight = 0;
 
     std::ifstream inputStream{"data.txt"};
     inputStream >> dataCount;
@@ -51,6 +53,17 @@ int main()
             std::cout<<std::endl;
         }
 
+        for(int task = 1; task<=taskCount; task++)
+        {
+            weight=0;
+            for(int mc=1; mc<=machineCount; mc++)
+            {
+                weight+=processTable[task-1][mc-1];
+            }
+            weightedTasks.push(std::make_pair(weight,task));
+        }
+
+        //wyzeruj usedTask
 
         tempOrder.push_back(0);
         for(int i=0;i<=taskCount;i++)
@@ -62,73 +75,53 @@ int main()
         for (int task = 1; task<= taskCount; task++)
         {
             min=INT32_MAX;
-            tempOrder.clear();
-            tempOrder.push_back(0);
-            tempOrder.insert(tempOrder.end(), order.begin(), order.end());
-
             for (int taskID = 1; taskID <= taskCount; taskID++)
-            {
-                if(usedTaskID[taskID]!=true)
+           {
+                if(usedTaskID[taskID]==true)
+                    continue;
+
+                for(int it=1;it<=task;it++ )
                 {
-                    if(taskID != 1)
-                    {
-                        tempOrder.clear();
-                        tempOrder.push_back(0);                        
-                    }
-                    tempOrder.push_back(taskID);
-                                        for (int i: tempOrder)
-            std::cout << i << ' ';
-                    //cmax = cmaxCalculate(order, task, machineCount);
-                    //bool used_taskID[]
-                    // if 0...
+                    tempOrder.clear();
+                    tempOrder.push_back(0);
+                    tempOrder.insert(tempOrder.end(), order.begin(), order.end());
+                    tempOrder.insert(tempOrder.begin()+it,taskID);
 
-                    /*cmaxTable[0][0]=processTable[taskID][0];
-                    for(int mc=1; mc<machineCount; mc++)
-                        cmaxTable[0][mc]=cmaxTable[0][mc-1] + processTable[0][mc];
-
-                    for(int i=1; i < task; i++)
-                        cmaxTable[order[i]][0]=cmaxTable[order[i-1]][0] + processTable[order[i]][0];*/
+                    for (int i: tempOrder)
+                        std::cout << i << ' ';
+                    std::cout<<std::endl;
 
                     for(int i = 1; i <= task; i++)
                         for(int mc = 1; mc <= machineCount; mc++)      
-                        {                  
-                            std::cout<<"Cmax order-1:"<<cmaxTable[ tempOrder[i-1] ][mc]<< " order: " << tempOrder[i]<<std::endl;
-                            std::cout<<"Cmax mc -1 :"<<cmaxTable[ tempOrder[i] ][mc-1]<<" mc: " << mc <<std::endl;
-                            std::cout<<"procTabl :"<<processTable[tempOrder[i]-1][mc-1]<<std::endl;
                             cmaxTable[ tempOrder[i] ][mc] = std::max(cmaxTable[ tempOrder[i-1] ][mc],cmaxTable[ tempOrder[i] ][mc-1]) + processTable[tempOrder[i]-1][mc-1];
-                            std::cout<<"cmaxTable :"<<cmaxTable[ tempOrder[i] ][mc]<<std::endl;
-                        }
 
                     cmax= cmaxTable[tempOrder[task]][machineCount];
-                    std::cout<<"Cmax:"<<cmax<<std::endl;
+                    //std::cout<<"Cmax:"<<cmax<<std::endl;
                     if(cmax<min)
                     {
                         if(min!=INT32_MAX)
-                        {
-                            usedTaskID[order.back()]=false;
-                            order.pop_back();
-                        }
-                        order.push_back(taskID);
+                            usedTaskID[minTaskID]=false;
+
+                        minTaskID=taskID;
                         min=cmax;
                         usedTaskID[taskID]=true;
-                    }
-                    else
-                        order.pop_back();
-                }
-            }
+                    }            
+                }   
+            }   
+            order.push_back(minTaskID);
         }
 
         
         for (int i: order)
             std::cout << i << ' ';
         std::cout << std::endl;
-        std::cout << cmaxTable[taskCount-1][machineCount-1] << std::endl;
+        std::cout << cmaxTable[order[taskCount-1]][machineCount] << std::endl;
 
         for(int task = 1; task<=taskCount; task++)
         {
             for(int mc=1; mc<=machineCount; mc++)
             {
-                std::cout<<cmaxTable[task][mc]<<" ";
+                std::cout<<cmaxTable[order[task-1]][mc]<<" ";
             }
             std::cout<<std::endl;
         }
