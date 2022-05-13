03451e876f12403c9169a5d89938bd7b6e1f9597
Kuba <kubaczarny3@gmail.com>
2022-05-13 18:49:09 +0200

    finished neh,qneh, basic shrage

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -19,202 +19,184 @@ struct myComp {
     }
 };
 
-void neh(std::ifstream &inputStream)
+ std::pair<int,std::vector<int>> neh(int processTable[500][20], int taskCount, int machineCount)
 {
-    int processTable[500][20];
     int cmaxTable[501][21];
     std::vector <int> order;
     std::vector <int> tempOrder;
+    std::pair <int, std::vector<int>> result;
     std::priority_queue <std::pair<int,int>, std::vector<std::pair<int, int>>, myComp> weightedTasks;
-    int dataCount, taskCount, machineCount, cmax, min, minPos, weight, taskID = 0;
-
-    inputStream >> dataCount;
+    int cmax, min, minPos, weight, taskID = 0;
 
 
-    for(int dt=0; dt<=dataCount; dt++)
+    for(int task = 1; task<=taskCount; task++)
     {
-        inputStream >> taskCount;
-        inputStream >> machineCount;
-        for(int task = 0; task<taskCount; task++)
-            for(int mc=0; mc<machineCount; mc++)
-                inputStream >> processTable[task][mc];
-
-        for(int task = 1; task<=taskCount; task++)
+        weight=0;
+        for(int mc=1; mc<=machineCount; mc++)
         {
-            weight=0;
-            for(int mc=1; mc<=machineCount; mc++)
-            {
-                weight+=processTable[task-1][mc-1];
-            }
-            weightedTasks.push(std::make_pair(weight,task));
+            weight+=processTable[task-1][mc-1];
         }
-
-
-        tempOrder.push_back(0);
-        for(int i=0;i<=taskCount;i++)
-            cmaxTable[i][0]=0;
-        for(int i=0;i<=machineCount;i++)
-            cmaxTable[0][i]=0;
-
-        for (int task = 1; task<= taskCount; task++)
-        {
-            min=INT32_MAX;
-
-            taskID=weightedTasks.top().second;
-            weightedTasks.pop();
-
-            for(int pos=1;pos<=task;pos++ )
-            {
-                tempOrder.clear();
-                tempOrder.push_back(0);
-                tempOrder.insert(tempOrder.end(), order.begin(), order.end());
-                tempOrder.insert(tempOrder.begin()+pos,taskID);
-
-                for(int i = 1; i <= task; i++)
-                    for(int mc = 1; mc <= machineCount; mc++)      
-                        cmaxTable[ tempOrder[i] ][mc] = std::max(cmaxTable[ tempOrder[i-1] ][mc],cmaxTable[ tempOrder[i] ][mc-1]) + processTable[tempOrder[i]-1][mc-1];
-                cmax= cmaxTable[tempOrder[task]][machineCount];
-
-                if(cmax<min)
-                {
-                    minPos=pos-1;
-                    min=cmax;
-                }            
-            }   
-              
-            order.insert(order.begin()+minPos,taskID);
-        }
-
-        for(int i = 0; i < taskCount; i++)
-                    for(int mc = 1; mc <= machineCount; mc++)      
-                        cmaxTable[ order[i] ][mc] = std::max(cmaxTable[ order[i-1] ][mc],cmaxTable[ order[i] ][mc-1]) + processTable[order[i]-1][mc-1];
-                cmax=cmaxTable[order[taskCount-1]][machineCount];
-
-
-        for (int i: order)
-            std::cout << i << ' ';
-        std::cout << std::endl;
-        std::cout << cmaxTable[order[taskCount-1]][machineCount] << std::endl;
-
-        order.clear();
-        tempOrder.clear();
+        weightedTasks.push(std::make_pair(weight,task));
     }
+    tempOrder.push_back(0);
+    for(int i=0;i<=taskCount;i++)
+        cmaxTable[i][0]=0;
+    for(int i=0;i<=machineCount;i++)
+        cmaxTable[0][i]=0;
+    for (int task = 1; task<= taskCount; task++)
+    {
+        min=INT32_MAX;
+        taskID=weightedTasks.top().second;
+        weightedTasks.pop();
+        for(int pos=1;pos<=task;pos++ )
+        {
+            tempOrder.clear();
+            tempOrder.push_back(0);
+            tempOrder.insert(tempOrder.end(), order.begin(), order.end());
+            tempOrder.insert(tempOrder.begin()+pos,taskID);
+            for(int i = 1; i <= task; i++)
+                for(int mc = 1; mc <= machineCount; mc++)      
+                    cmaxTable[ tempOrder[i] ][mc] = std::max(cmaxTable[ tempOrder[i-1] ][mc],cmaxTable[ tempOrder[i] ][mc-1]) + processTable[tempOrder[i]-1][mc-1];
+            cmax= cmaxTable[tempOrder[task]][machineCount];
+            if(cmax<min)
+            {
+                minPos=pos-1;
+                min=cmax;
+            }            
+        }   
+          
+        order.insert(order.begin()+minPos,taskID);
+    }
+    for(int i = 0; i < taskCount; i++)
+                for(int mc = 1; mc <= machineCount; mc++)      
+                    cmaxTable[ order[i] ][mc] = std::max(cmaxTable[ order[i-1] ][mc],cmaxTable[ order[i] ][mc-1]) + processTable[order[i]-1][mc-1];
+            cmax=cmaxTable[order[taskCount-1]][machineCount];
 
+    result=std::make_pair(cmaxTable[order[taskCount-1]][machineCount], order);
+    
+    return result;
 }
 
-void qneh(std::ifstream &inputStream)
+std::pair<int,std::vector<int>> qneh(int processTable[500][20], int taskCount, int machineCount)
 {
-    int processTable[500][20];
     int cmaxTable[501][21];
     int cmaxTableBackward[501][21];
     int tempCmax[20];
     std::vector <int> order;
+    std::pair <int, std::vector<int>> result;
     std::priority_queue <std::pair<int,int>, std::vector<std::pair<int, int>>, myComp> weightedTasks;
-    int dataCount, taskCount, machineCount, cmax, min, minPos, weight, taskID = 0;
+    int cmax, min, minPos, weight, taskID = 0;
 
-    
-    inputStream >> dataCount;
 
-    for(int i=0;i<501;i++)
+    for(int i=0;i<=taskCount;i++)
         cmaxTable[i][0]=0;
-    for(int i=0;i<21;i++)
+    for(int i=0;i<=machineCount;i++)
         cmaxTable[0][i]=0;
 
-    for(int i=0;i<501;i++)
-        for(int j=0;j<21;j++)
+    for(int i=0;i<=taskCount;i++)
+        for(int j=0;j<=machineCount;j++)
             cmaxTableBackward[i][j]=0;
       
-    for(int dt=0; dt<=dataCount; dt++)
-    {
-        inputStream >> taskCount;
-        inputStream >> machineCount;
-        for(int task = 0; task<taskCount; task++)
-            for(int mc=0; mc<machineCount; mc++)
-                inputStream >> processTable[task][mc];
         
-        for(int task = 1; task<=taskCount; task++)
+    for(int task = 1; task<=taskCount; task++)
+    {
+        weight=0;
+        for(int mc=1; mc<=machineCount; mc++)
         {
-            weight=0;
-            for(int mc=1; mc<=machineCount; mc++)
-            {
-                weight+=processTable[task-1][mc-1];
-            }
-            weightedTasks.push(std::make_pair(weight,task));
+            weight+=processTable[task-1][mc-1];
         }
-
-        order.push_back(0);
-        order.push_back(weightedTasks.top().second);
-        weightedTasks.pop();
-        order.push_back(0);
-
-        for (int task = 1; task< taskCount; task++)
-        {
-            min=INT32_MAX;
-
-            taskID=weightedTasks.top().second;
-            weightedTasks.pop();
-
-            for(int i = 1; i <= task; i++)
-                for(int mc = 1; mc <= machineCount; mc++)      
-                    cmaxTable[ order[i] ][mc] = std::max(cmaxTable[ order[i-1] ][mc],cmaxTable[ order[i] ][mc-1]) + processTable[order[i]-1][mc-1];
-
-            for(int i = task; i > 0 ; i--)
-                for(int mc = machineCount; mc > 0 ; mc--)      
-                    cmaxTableBackward[ order[i] ][mc-1] = std::max(cmaxTableBackward[ order[i+1] ][mc-1],cmaxTableBackward[ order[i] ][mc]) + processTable[order[i]-1][mc-1];
-
-
-            for(int pos=0;pos<=task;pos++ )
-            {
-                tempCmax[0]= cmaxTable[ order[pos] ][ 1 ] + processTable[taskID-1][0];
-                
-                for(int mc=1;mc<machineCount;mc++)
-                    tempCmax[mc]= std::max(cmaxTable[order[pos]][mc+1],tempCmax[mc-1]) + processTable[taskID-1][mc];
-
-                for(int mc=0; mc<machineCount; mc++)
-                    cmax= std::max(cmax , cmaxTableBackward[ order[pos+1] ][mc] + tempCmax[mc] );
-
-                if(cmax<min)
-                {
-                    minPos=pos+1;
-                    min=cmax;
-                }  
-                cmax=0;        
-            }    
-            order.insert(order.begin()+minPos,taskID);
-        }
-
-        std::cout<<"min: "<<min<<std::endl;  
-
-        for(int i=1; i<order.size()-1;i++)
-          std::cout << order[i] << ' ';
-        std::cout << std::endl;
-        order.clear();
-
+        weightedTasks.push(std::make_pair(weight,task));
     }
+    order.push_back(0);
+    order.push_back(weightedTasks.top().second);
+    weightedTasks.pop();
+    order.push_back(0);
+    for (int task = 1; task< taskCount; task++)
+    {
+        min=INT32_MAX;
+        taskID=weightedTasks.top().second;
+        weightedTasks.pop();
+        for(int i = 1; i <= task; i++)
+            for(int mc = 1; mc <= machineCount; mc++)      
+                cmaxTable[ order[i] ][mc] = std::max(cmaxTable[ order[i-1] ][mc],cmaxTable[ order[i] ][mc-1]) + processTable[order[i]-1][mc-1];
+        for(int i = task; i > 0 ; i--)
+            for(int mc = machineCount; mc > 0 ; mc--)      
+                cmaxTableBackward[ order[i] ][mc-1] = std::max(cmaxTableBackward[ order[i+1] ][mc-1],cmaxTableBackward[ order[i] ][mc]) + processTable[order[i]-1][mc-1];
+        for(int pos=0;pos<=task;pos++ )
+        {
+            tempCmax[0]= cmaxTable[ order[pos] ][ 1 ] + processTable[taskID-1][0];
+            
+            for(int mc=1;mc<machineCount;mc++)
+                tempCmax[mc]= std::max(cmaxTable[order[pos]][mc+1],tempCmax[mc-1]) + processTable[taskID-1][mc];
+            for(int mc=0; mc<machineCount; mc++)
+                cmax= std::max(cmax , cmaxTableBackward[ order[pos+1] ][mc] + tempCmax[mc] );
+            if(cmax<min)
+            {
+                minPos=pos+1;
+                min=cmax;
+            }  
+            cmax=0;        
+        }    
+        order.insert(order.begin()+minPos,taskID);
+    }
+    order.erase(order.begin());
+    order.pop_back();
+
+    result=std::make_pair(min,order);  
+
+    return result;
 }
 
 
 int main()
 {
+    int dataCount, taskCount, machineCount, nehTime, qnehTime, nehAbsoluteTime, qnehAbsoluteTime = 0;
+    int processTable[500][20];
+    std::pair <int, std::vector<int>> result;
+    std::chrono::_V2::system_clock::time_point start,stop;
+    std::chrono::microseconds duration;
+
     std::ifstream inputStream{"data.txt"};
-    int nehTime, qnehTime=0;
-    auto start = std::chrono::high_resolution_clock::now();
-    neh(inputStream);
-    auto stop = std::chrono::high_resolution_clock::now(); 
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    nehTime=duration.count();
 
-    inputStream.clear();                
-    inputStream.seekg(0, std::ios::beg);
+    inputStream >> dataCount;
 
-    start = std::chrono::high_resolution_clock::now();
-    qneh(inputStream);
-    stop = std::chrono::high_resolution_clock::now(); 
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    qnehTime=duration.count();
+    for(int dt=0; dt<=dataCount; dt++)
+    {
+        inputStream >> taskCount;
+        inputStream >> machineCount;
+        for(int task = 0; task<taskCount; task++)
+            for(int mc=0; mc<machineCount; mc++)
+                inputStream >> processTable[task][mc];
+        std::cout << "NEH data" << dt <<std::endl;
+        start = std::chrono::high_resolution_clock::now();
+        result=neh(processTable,taskCount,machineCount);
+        stop = std::chrono::high_resolution_clock::now(); 
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        nehTime=duration.count();
+        for (int i: result.second)
+                std::cout << i << ' ';
+        std::cout << std::endl;
+        std::cout << "MinCmax: " << result.first << std::endl;
+        std::cout << "Czas trwania: " << nehTime << std::endl << std::endl;
+        nehAbsoluteTime+=nehTime;
 
-    std::cout<< "Czas trwanie neh: "<< nehTime << " mikrosekund." <<std::endl;
-    std::cout<< "Czas trwanie qneh: "<< qnehTime << " mikrosekund."<<std::endl;
+
+        std::cout << "QNEH data" << dt <<std::endl;
+        start = std::chrono::high_resolution_clock::now();
+        result=qneh(processTable,taskCount,machineCount);
+        stop = std::chrono::high_resolution_clock::now(); 
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        qnehTime=duration.count();
+        for (int i: result.second)
+                std::cout << i << ' ';
+        std::cout << std::endl;
+        std::cout << "MinCmax: " << result.first << std::endl;
+        std::cout << "Czas trwania: " << qnehTime << std::endl << std::endl;
+        qnehAbsoluteTime+=qnehTime;
+    }
+
+    std::cout<< "Calkowity czas trwanie neh: "<< nehAbsoluteTime << " mikrosekund." <<std::endl;
+    std::cout<< "Calkowity czas trwanie qneh: "<< qnehAbsoluteTime << " mikrosekund."<<std::endl;
     
     return 0;
 }
